81375a23451db47a4d58b9eea2fe315da5cf266b
eldar <eldarphp@ya.ru>
2018-05-19 14:07:30 +0300

    Add Vue acrions (v-bind, v-on:click, v-if)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 
 const car = (name, model, owner, year, phone, image) => ({name, model, owner, year, phone, image})
-
-new Vue({
-	el: '#app',
-	data: {
-		cars: [
+const cars = [
 			car('Chevrolet', 'Camaro', 'Dom', '2002', '+7 987 654 32 10', 'cars/chevrolet.png'),
 			car('Volkswagen', 'SR-500', 'Sam', '2000', '+7 999 788 20 20', 'cars/volkswagen.png'),
 			car('Audi', 'A-5', 'Din', '2010', '+7 722 523 75 88', 'cars/audi.png'),
 		]
+
+new Vue({
+	el: '#app',
+	data: {
+		cars: cars,
+		car: cars[0],
+		selectedCarIndex: 0,
+		phoneVisibility: false
+	},
+	methods: {
+		selectCar: function(index){
+			this.car = cars[index],
+			this.selectedCarIndex = index
+		}
 	}
 })
